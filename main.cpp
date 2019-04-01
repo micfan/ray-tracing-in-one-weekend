5c1eaa5a4fdca1fd442072e25341c7059a76cd44
Micfan <2443027+micfan@users.noreply.github.com>
2019-04-02 00:28:37 +0800

    add chapter 1: Output an image

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+int main() {
+    std::ofstream out("ch1.ppm");
+    int nx = 200;
+    int ny = 100;
+    out << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            out << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    out.close();
+
+    return EXIT_SUCCESS;
+}
