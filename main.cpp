7d114e6709bcc2163f7b2977d40d22f0902bd7a4
Micfan <2443027+micfan@users.noreply.github.com>
2019-04-02 20:27:31 +0800

    add Chapter 3: Rays, a simple camera, and background

--- main.cpp
+++ main.cpp
@@ -2,14 +2,26 @@
 #include <string>
 #include <fstream>
 #include "vec3.hpp"
+#include "ray.hpp"
 
 using namespace std;
 
+vec3 color(const ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3{1.0, 1.0, 1.0} + t * vec3{0.5, 0.7, 1.0};
+}
 
 int main() {
-    auto filename = "ch2.ppm";
+    auto filename = "ch3.ppm";
     std::ofstream out(filename);
     cout << "output image: " << filename << endl;
+
+    vec3 lower_left_corner{-2.0, -1.0, -1.0};
+    vec3 horizontal{4.0, 0.0, 0.0};
+    vec3 vertical{0.0, 2.0, 0.0};
+    vec3 origin{0.0, 0.0, 0.0};
 
     int nx = 200;
     int ny = 100;
@@ -17,9 +29,10 @@ int main() {
 
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            vec3 col(floor(i) / floor(nx),
-                     floor(j) / floor(ny),
-                     0.2);
+            double u = double(i) / double(nx);
+            double v = double(j) / double(ny);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
---
+++ ray.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "vec3.hpp"
+
+class ray
+{
+public:
+    ray() = default;
+    ray(const vec3& a, const vec3& b) : A(a), B(b) {}
+
+    vec3 origin() const { return A; }
+    vec3 direction() const { return B; }
+    vec3 point_at_parameter(double t) const { return A + t*B; }
+
+    vec3 A;
+    vec3 B;
+};
