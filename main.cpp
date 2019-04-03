1d7004af2741262c071ef548a7325f3b65e511f0
Micfan <2443027+micfan@users.noreply.github.com>
2019-04-03 20:59:25 +0800

    add Chapter 5: Surface normals and multiple objects, Part 1

--- main.cpp
+++ main.cpp
@@ -6,29 +6,39 @@
 
 using namespace std;
 
-bool hit_sphere(const vec3& center, double radius, const ray& r)
+double hit_sphere(const vec3& center, double radius, const ray& r)
 {
     vec3 oc = r.origin() - center;
     double a = dot(r.direction(), r.direction());
     double b = 2.0 * dot(oc, r.direction());
     double c = dot(oc, oc) - radius*radius;
     double discriminant = b*b -4*a*c;
-    return discriminant > 0;
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 vec3 color(const ray& r)
 {
-    if (hit_sphere(vec3{0, 0, -1}, 0.5, r))
-    {
-        return vec3(1, 0, 0);
+    double t = hit_sphere(vec3{0, 0, -1}, 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3{0, 0, -1});
+        return 0.5 * vec3{
+            N.x() + 1,
+            N.y() + 1,
+            N.z() + 1
+        };
     }
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * vec3{1.0, 1.0, 1.0} + t * vec3{0.5, 0.7, 1.0};
 }
 
 int main() {
-    auto filename = "ch4.ppm";
+    auto filename = "ch5.part1.ppm";
     std::ofstream out(filename);
     cout << "output image: " << filename << endl;
 
