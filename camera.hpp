ca5a39dbbc2c45f9b96599339e6ed102cb001403
Micfan <2443027+micfan@users.noreply.github.com>
2020-11-18 18:20:40 +0800

    add Chapter 10: Positionable camera, part 2

--- camera.hpp
+++ camera.hpp
@@ -6,12 +6,22 @@
 class camera
 {
 public:
-    camera()
-        : lower_left_corner(vec3{-2.0, -1.0, -1.0})
-        , horizontal(vec3{4.0, 0.0, 0.0})
-        , vertical(vec3{0.0, 2.0, 0.0})
-        , origin(vec3{0.0, 0.0, 0.0})
-    {}
+	camera(vec3 lookfrom, vec3 lookat, vec3 vup, double vfov, double aspect)
+	{
+		double theta = vfov*M_PI/180;
+		double half_height = tan(theta/2);
+		double half_width = aspect * half_height;
+
+		origin = lookfrom;
+		vec3 w = unit_vector(lookfrom - lookat);
+		vec3 u = unit_vector(cross(vup, w));
+		vec3 v = cross(w, u);
+
+		lower_left_corner = vec3{-half_width, -half_height, -1.0};
+		lower_left_corner = origin - half_width*u - half_height*v - w;
+		horizontal = 2*half_width*u;
+		vertical = 2*half_height*v;
+	}
 
     ray get_ray(double u, double v)
     {
--- main.cpp
+++ main.cpp
@@ -56,25 +56,27 @@ vec3 color(const ray& r, std::shared_ptr<hitable> world, int depth)
 }
 
 int main() {
-    auto filename = "ch10.part1.ppm";
+    auto filename = "ch10.part2.ppm";
     std::ofstream out(filename);
     cout << "output image: " << filename << endl;
 
-    camera cam;
+    camera cam{
+    	vec3(-2, 2, 1),
+		vec3(0, 0, -1),
+		vec3(0, 1, 0),
+		90,
+		double(16)/double(9)
+    };
 
     auto list = std::vector<hitable_ptr>();
 
-//    list.push_back(std::make_shared<sphere>(vec3{0, 0, -1}, 0.5, std::make_shared<lambertian>(vec3{0.1, 0.2, 0.5})));
-//    list.push_back(std::make_shared<sphere>(vec3{0, -100.5, -1}, 100, std::make_shared<lambertian>(vec3{0.8, 0.8, 0.0})));
-//    list.push_back(std::make_shared<sphere>(vec3{1, 0, -1}, 0.5, std::make_shared<metal>(vec3{0.8, 0.6, 0.2})));
-//    list.push_back(std::make_shared<sphere>(vec3{-1, 0, -1}, 0.5, std::make_shared<dielectric>(1.5)));
-//    list.push_back(std::make_shared<sphere>(vec3{-1, 0, -1}, -0.45, std::make_shared<dielectric>(1.5)));
+    list.push_back(std::make_shared<sphere>(vec3{0, 0, -1}, 0.5, std::make_shared<lambertian>(vec3{0.1, 0.2, 0.5})));
+    list.push_back(std::make_shared<sphere>(vec3{0, -100.5, -1}, 100, std::make_shared<lambertian>(vec3{0.8, 0.8, 0.0})));
+    list.push_back(std::make_shared<sphere>(vec3{1, 0, -1}, 0.5, std::make_shared<metal>(vec3{0.8, 0.6, 0.2})));
+    list.push_back(std::make_shared<sphere>(vec3{-1, 0, -1}, 0.5, std::make_shared<dielectric>(1.5)));
+    list.push_back(std::make_shared<sphere>(vec3{-1, 0, -1}, -0.45, std::make_shared<dielectric>(1.5)));
 
-    double R = cos(M_PI/4);
-	list.push_back(std::make_shared<sphere>(vec3{-R, 0, -1}, R, std::make_shared<lambertian>(vec3(0, 0, 1))));
-	list.push_back(std::make_shared<sphere>(vec3{R, 0, -1}, R, std::make_shared<lambertian>(vec3(1, 0, 0))));
-
-    auto world = std::make_shared<hitable_list>(list, 2);
+    auto world = std::make_shared<hitable_list>(list, 5);
 
     int nx = 200;
     int ny = 100;
